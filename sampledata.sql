1c08c3c77a78050dd8a6ff0d66a41be9383fa52c
tommy8492nd <tommy8492nd@gmail.com>
2017-05-10 17:37:01 -0700

    Added Sampledata.sql
    This contains the sample data for the tables.
    (NOT COMPLETE)

---
+++ sampledata.sql
@@ -0,0 +1,28 @@
+INSERT INTO MaintainPackage
+VALUES
+    (1, 'Engine Check Package', 'Investigate engine issues'),
+    (2, 'ECU Check', 'Investigate ECU corruptions or faults'),
+    (3, 'Transmission Check Package', 'Investigate transmission issues')
+;
+
+INSERT INTO AppointmentStatus
+VALUES
+    ('FUTURE'),
+    ('APPOINTMENT KEPT'),
+    ('CANCELLED')
+;
+
+INSERT INTO CommunicationType
+VALUES
+    ('Email'),
+    ('Phone Call')
+;
+
+INSERT INTO Customer
+VALUES
+    (99, 'Timmy', 'Carder', '1982-09-22', '329-323-4512', 'nottheprez@gmail.com'),
+    (98, 'Haddam', 'Sussein', '1967-11-01', '323-315-9695', 'bestkingevar@gmail.com'),
+    (97, 'Matoi', 'Ryuko', '1997-01-02', '928-888-8251', 'sailorclothes@gmail.com'),
+    (96, 'Celino', 'Barnes', '1983-08-15', '800-888-8888', 'injuryatturnyfam@hotmail.com'),
+    (95, 'Imi', 'Mopkins', '1965-03-02', '123-456-7890', 'reelprufessurlelz@gmail.com')
+;
